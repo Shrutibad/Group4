469e33db7a01444c5bceafaea0627dc1a999a90c
Shrutibad <31158027+Shrutibad@users.noreply.github.com>
2017-11-03 08:48:30 +0530

    Create img_metric.py

---
+++ img_metric.py
@@ -0,0 +1,11 @@
+#to convert an image to metric
+
+#importing liraries
+from scipy import misc
+import numpy as np
+
+#this function converts a given image to metric
+image = misc.imread('minion.jpg')
+
+#prints metric
+print (image)
